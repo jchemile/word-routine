f7c48a9471b7d10a527eeda2abc44c4e6e81bdac
HOME\jchemile <joaquinchemile@gmail.com>
2021-11-29 16:39:43 -0300

    first version

---
+++ entries/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+    languaje = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Entries"
